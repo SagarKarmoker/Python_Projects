2745c39a9fae43e72186eb5908a8519909aca87a
Sagar Karmoker <98447327+SagarKarmoker@users.noreply.github.com>
2023-08-07 06:53:54 +0000

    updated qr

--- QRcode/qr.py
+++ QRcode/qr.py
@@ -1,14 +1,18 @@
 import streamlit as st
 import qrcode
 from PIL import Image
+import os
 
 st.title("QR Code Generator")
 
 data = st.text_input("Your text/data")
+if data is not None:
+    img = qrcode.make(data)
+    type(img)  # qrcode.image.pil.PilImage
+    img.save("_pic.png")
 
-img = qrcode.make(data)
-type(img)  # qrcode.image.pil.PilImage
-img.save("_pic.png")
+    image = Image.open('_pic.png')  # Corrected file extension
+    st.image(image, caption='Your QR Code')
 
-image = Image.open('_pic.png')  # Corrected file extension
-st.image(image, caption='Your QR Code')
+    # Remove the generated image file
+    os.remove('_pic.png')
